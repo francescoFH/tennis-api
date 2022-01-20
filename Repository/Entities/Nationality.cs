bf481104e4b012a1903cd7b3da6483662122b4cd
Brendan Roberts <Brendan.Roberts@afsi.com>
2022-01-20 14:49:03 +0000

    First Commit

---
+++ Repository/Entities/Nationality.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Repository.Entities
+{
+    public class Nationality
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Repository/Entities/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Repository.Entities
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int NationalityId { get; set; }
+
+        public Nationality Nationality { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int Points { get; set; }
+
+        public int Games { get; set; }
+    }
+}
